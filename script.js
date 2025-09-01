7cc186ace9b03188ca00950a15724b39c4465978
RiccardoOttavi <ottavi.ps@libero.it>
2025-09-01 20:52:23 +0200

    finalizza ricerca mail

--- script.js
+++ script.js
@@ -1,2 +1,18 @@
 let mailList = ["mariorossi@gmail.it", "carloverdi@gmail.com", "giuliobianchi@gmail.com"];
 let userMail = prompt("Inserisci qui la tua mail");
+
+
+let allowLogin = false;
+for (let i = 0 ; i < mailList.length; i++){
+    if (mailList[i] === userMail){
+        allowLogin = true;
+    }else {
+        allowLogin = false;       
+    }
+}
+
+if (allowLogin === true) {
+    console.log("Benvenuto!");
+}else {
+    console.log("Accesso negato!");
+}
