f451bf08123e28d24d41f2832f7f49b4e94eed67
RiccardoOttavi <ottavi.ps@libero.it>
2025-09-01 22:51:19 +0200

    finalizza la logica del gioco

--- dadi/script.js
+++ dadi/script.js
@@ -1,4 +1,15 @@
 let cpuNum = Math.random()* 1 * 6;
 let playerNum = Math.random() * 6;
-console.log(cpuNum.toFixed(0));
-console.log(playerNum.toFixed(0));
+
+
+console.log("Tiri un dado, il risultato è: " + playerNum.toFixed(0));
+console.log("Il tuo avversario tira un dado, il risultato è: " + cpuNum.toFixed(0));
+
+if (playerNum > cpuNum) {
+    console.log("Hai vinto!");
+}else if (cpuNum > playerNum) {
+    console.log("Hai perso!");
+}else {
+    console.log("Pareggio!")
+}
+
